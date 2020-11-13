b51bfeb5b0b5827ad19c4805e4f3f064e208189e
Kevin Williams <kupo.kev@gmail.com>
2020-11-12 23:33:38 -0600

    initial code push

---
+++ XlsxToSql/Program.cs
@@ -0,0 +1,16 @@
+﻿using XlsxToSql.Services;
+
+namespace XlsxToSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "F:\\Test\\Test.xlsx";
+
+            var importService = new XlsxImportService();
+
+            importService.ImportSpreadsheet(filepath);
+        }
+    }
+}
---
+++ XlsxToSql/Services/XlsxImportService.cs
@@ -0,0 +1,36 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XlsxToSql.Services
+{
+    public class XlsxImportService
+    {
+        public void ImportSpreadsheet(string filepath)
+        {
+            var columns = new Dictionary<string, int>();
+
+            using(ExcelPackage package = new ExcelPackage(new System.IO.FileInfo(filepath)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+
+                // Get column names
+                for( int i = worksheet.Dimension.Start.Column; i <= worksheet.Dimension.End.Column; i++)
+                {
+                    string key = worksheet.Cells[1, i].Value == null ? "" : worksheet.Cells[1, i].Value.ToString();
+
+                    if (!string.IsNullOrWhiteSpace(key) && !columns.ContainsKey(key))
+                    {
+                        key = key.ToLower().Replace("-", "_");
+
+                        columns.Add(key, 0);
+                    }
+                }
+
+                // Get column lengths
+
+                System.Console.WriteLine(columns.ToString());
+            }
+        }
+    }
+}
